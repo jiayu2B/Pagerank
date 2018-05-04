5e38f12e4ec821f7b37668e9209e5be282295bc5
jiayu2B <jiayukmifeng@qq.com>
2018-05-03 20:25:21 -0700

    Add files via upload

--- pagerank/main.py
+++ pagerank/main.py
@@ -5,26 +5,40 @@ def Score(Matrix, weight):
     return res
 
 '''          A   B   C   D   E   F    '''
-Matrix = [[  0,  0,  0,  0,  0,1/5],
-          [  1,  0,1/3,1/3,1/4,1/5],
-          [  0,1/3,  0,1/3,1/4,1/5],
-          [  0,1/3,1/3,  0,1/4,1/5],
-          [  0,1/3,1/3,1/3,  0,1/5],
+'''Matrix = [[  0,  0,  0,  0,  0,  0],
+          [  1,  0,1/3,1/3,1/4,  0],
+          [  0,1/3,  0,1/3,1/4,  0],
+          [  0,1/3,1/3,  0,1/4,  0],
+          [  0,1/3,1/3,1/3,  0,  0],
           [  0,  0,  0,  0,1/4,  0]
-          ]
-          
+          ]'''
+f = open('graph.txt')
+Matrix_ori = f.readline().split(',')
+Matrix = []
+a = [0,0,0,0,0,0]
+for i in range(len(Matrix_ori)):
+
+    a[i%6] = float(Matrix_ori[i])
+    if i%6 == 5:
+        Matrix.append(a)
+        a = [0,0,0,0,0,0]
+    
+
 Page = []
 for i in range(len(Matrix)):
     Page.append(100/len(Matrix))
 
 itera = 0
-while (itera < 20):
-    print(itera)
+while (True):
+
     itera+=1
     new_page = []
     for i in range(len(Matrix)):
         tem = Score(Matrix[i],Page)
         new_page.append(tem)
+    if Page == new_page:
+        break;
     Page = new_page
-    print(Page)
-print(Page)
+
+print("Page Score is:",Page)
+print("Iteration time:",itera)
